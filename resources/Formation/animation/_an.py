e8cdacd0f57c8a3a985437e287e6c9eba1f25b47
ضـُِﯡضِاެءَ <102345602+Dawdan@users.noreply.github.com>
2022-06-24 05:42:27 +0300

    Add files via upload

---
+++ resources/Formation/animation/_an.py
@@ -0,0 +1,13 @@
+#   RedThon - USERBOT
+#   TELE - @Redthon
+
+#  ======================================================= #
+
+from userbot.Config import Config
+
+USERID = Config.OWNER_ID 
+ALIVE_NAME = Config.ALIVE_NAME
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Redthon Userbot"
+mention = f"[{DEFAULTUSER}](tg://user?id={USERID})"
+
+#  ======================================================= #
